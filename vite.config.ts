8496e23d6797ec7f4376f525752011381cab905e
tphl6256TTT <tphl6256@gmail.com>
2025-12-01 11:41:03 +0800

    Set Vite base to /gamebridge/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/da_bridge/',
+    base: '/gamebridge/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
